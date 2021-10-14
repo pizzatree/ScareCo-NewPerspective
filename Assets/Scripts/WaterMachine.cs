5994aac70ead43fde8ae8717e8fc15988de3eda0
Chris <55300770+pizzatree@users.noreply.github.com>
2021-10-13 23:38:51 -0500

    Added Machines & Beans
    Added Water Machine
    - Dispenses waters with a physical press (or smack) of the button
    - New Model
    
    Added Bean Machine
    - Dispenses beans when given quarters
    
    Added beans, quarters, and necessary sounds / graphics.

---
+++ Assets/Scripts/WaterMachine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMachine : MonoBehaviour
+{
+    [Header("Water Spawn")]
+    [SerializeField] private GameObject waterPrefab;
+    [SerializeField] private Transform  spawnPoint;
+
+    [Header("Button")]
+    [SerializeField] private Transform button;
+    [SerializeField] private float     buttonThreshold = 0.1f;
+
+    private bool    isActivated;
+    private Vector3 buttonStartPos;
+
+    private void Start()
+    {
+        buttonStartPos = button.localPosition;
+    }
+
+    private void Update()
+    {
+        var buttonActuated = Mathf.Abs(Vector3.Distance(buttonStartPos, button.localPosition)) > buttonThreshold;
+        
+        if(isActivated && buttonActuated)
+            return;
+        
+        if(buttonActuated)
+        {
+            isActivated = true;
+            DispenseWater();
+            return;
+        }
+
+        isActivated = false;
+    }
+
+    public void DispenseWater()
+    {
+        Instantiate(waterPrefab, spawnPoint.position, transform.rotation);
+    }
+}
