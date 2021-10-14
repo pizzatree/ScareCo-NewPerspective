6512116dc173e668882f8a59e2ae1a57498bc192
Gabe <thetiemann01@gmail.com>
2021-10-14 16:01:50 -0500

    water particle on the prefab

---
+++ Assets/Scripts/WaterPour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPour : MonoBehaviour
+{
+    ParticleSystem waterParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waterParticle = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        waterParticle.Play();
+        if(Vector3.Angle(Vector3.down, transform.up) <= 90f){
+            waterParticle.Play();
+        }
+        else {
+            waterParticle.Stop();
+        }
+    }
+}
