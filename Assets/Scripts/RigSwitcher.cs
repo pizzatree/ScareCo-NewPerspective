cc6bd4c2a893395f0ff75e2c7f44ac0a026fc559
Chris <55300770+pizzatree@users.noreply.github.com>
2021-10-15 11:53:34 -0500

    Added RigSwitcher
    When in the editor, the simulator will be active
    All other contexts run the XR rig

---
+++ Assets/Scripts/RigSwitcher.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RigSwitcher : MonoBehaviour
+{
+    [SerializeField] private GameObject simulator, XR;
+    private void Start()
+    {
+        simulator.SetActive(Application.isEditor);
+        XR.SetActive(!Application.isEditor);
+    }
+}
