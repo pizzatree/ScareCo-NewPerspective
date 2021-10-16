a7c76a6f5d16df2c361f262bd7a8f59fcc198969
Chris <55300770+pizzatree@users.noreply.github.com>
2021-10-16 15:53:37 -0500

    Added a detailed outdoors scene

---
+++ Assets/Scripts/FloatingObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObject : MonoBehaviour
+{
+    [SerializeField] private float speed    = 3f;
+    [SerializeField] private float distance = .35f;
+
+    private Vector3 startingPos;
+
+    private void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = startingPos + new Vector3(0f, Mathf.Sin(Time.time * speed) * distance, 0f);
+    }
+}
