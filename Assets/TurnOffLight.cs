c6f489f71e40f6081a44bfb2f381c33345a07577
Gabe <thetiemann01@gmail.com>
2021-11-27 13:28:29 -0600

    the lights woooork (only if you press up/down keys

---
+++ Assets/TurnOffLight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine;
+
+public class TurnOffLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.UpArrow))
+            this.GetComponent<Light> ().enabled = true;
+        if (Input.GetKey (KeyCode.DownArrow))
+            this.GetComponent<Light> ().enabled = false;
+    }
+}
