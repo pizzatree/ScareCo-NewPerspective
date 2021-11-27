64d3a4485e3ab1d72eb472be7addd0669af31fe0
Chris <55300770+pizzatree@users.noreply.github.com>
2021-11-27 14:13:56 -0600

    Added SceneSwitcher
    WorldChanger
    - Supports an interface to swap how scenes are being switched, right now just outputs desired scene
    
    Level Switch Prefab
    - Ray activated button to switch scenes
    
    SceneToSwitchButton
    - Placed on Level Switch Prefab to give an enum for changing the scene

---
+++ Assets/Scripts/SwitchToSceneButton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class SwitchToSceneButton : MonoBehaviour
+{
+    public Scenes scene;
+
+    private void OnValidate()
+    {
+        name = $"{scene.ToString()} button";
+        transform.Find("Title").GetComponent<TMP_Text>().text = scene.ToString();
+    }
+}
---
+++ Assets/Scripts/WorldChanger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldChanger : MonoBehaviour
+{
+    private ISceneChanger sceneChanger = new SceneChangerDebugLog();
+
+    public void ChangeScene(SwitchToSceneButton switchTo)
+        => sceneChanger.SwitchTo(switchTo.scene);
+}
+
+public class SceneChangerDebugLog : ISceneChanger
+{
+    public void SwitchTo(Scenes scene)
+        => Debug.Log(scene.ToString());
+}
+
+public class SceneChangerSceneLoad : ISceneChanger
+{
+    public void SwitchTo(Scenes scene)
+        => SceneManager.LoadScene(scene.ToString());
+}
+
+public class SceneChangerAdditive : ISceneChanger
+{
+    public void SwitchTo(Scenes scene)
+        => Debug.LogWarning("Need to implement Additive Loading");
+}
+
+public interface ISceneChanger
+{
+    void SwitchTo(Scenes scene);
+}
+
+public enum Scenes
+{
+    Original,
+    MegoPlayset,
+    LandOfGiants,
+    DancingOnCeiling,
+    NoGravity
+}
